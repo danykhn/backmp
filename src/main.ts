4f43f6f673f580de19c9a0380251e1a2d406c2ee
danykhn <danjel2014@gmail.com>
2023-11-10 15:28:40 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +10,6 @@ async function bootstrap() {
     //origin: ['https://app.miclinicamedica.com.ar/', 'http://app.miclinicamedica.com.ar/']
   });
   const PORT = process.env.port || 3091;
-  await app.listen(PORT, () => Logger.log(`Server started on http://localhost:${PORT}/api/doc`));
+  await app.listen(PORT);
 }
 bootstrap();
